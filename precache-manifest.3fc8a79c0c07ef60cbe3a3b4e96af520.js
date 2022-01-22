9fb9aa9d32e13df61f80059984dfbd8b0056632f
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2022-01-22 00:20:06 -0300

    Updates

---
+++ precache-manifest.3fc8a79c0c07ef60cbe3a3b4e96af520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85de4abfefef3b2bd22d79d3809cd91a",
+    "url": "/projeto-a/index.html"
+  },
+  {
+    "revision": "3fe0fc758cada32ba254",
+    "url": "/projeto-a/static/css/main.e67addbe.chunk.css"
+  },
+  {
+    "revision": "10ae0603604359519bfb",
+    "url": "/projeto-a/static/js/2.50c414a1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/projeto-a/static/js/2.50c414a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe0fc758cada32ba254",
+    "url": "/projeto-a/static/js/main.d07028a0.chunk.js"
+  },
+  {
+    "revision": "b72ee5cddcbcf09f8bab",
+    "url": "/projeto-a/static/js/runtime-main.3c925c51.js"
+  }
+]);
